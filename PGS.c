d451f5275eec662de5e7d4e5fb081795ac9351d0
AaronMcGarry <aaronmcgarry42@gmail.com>
2025-05-14 15:50:08 -0500

    refactor states in pgs

--- PGS.c
+++ PGS.c
@@ -18,17 +18,12 @@ int main (int argc, char *argv[]){
     int write_pipe = atoi(argv[2]);
     int num_racks = atoi(argv[3]);
 
-    // -1 = stop, 1 = start, 0 = no change
-    int *fans = malloc(num_racks * sizeof *fans);
-    int *power = malloc(num_racks * sizeof *power);
+    // 0 = no change; 2 = turn off power; 3 = turn on fan; 4 = turn off fan.
+    // turning on power is handled by PES
+    int *plans = malloc(num_racks * sizeof *plans);
 
-    for (int i = 0; i < num_racks; i++) {
-        fans[i] = 0;
-        power[i] = 1;
-    }
-
-    float *buffer = malloc(2 * num_racks * sizeof *buffer);
-    int *write_buffer = malloc(2 * num_racks * sizeof *buffer);
+    float *buffer = malloc(num_racks * sizeof *buffer);
+    int *write_buffer = malloc(num_racks * sizeof *buffer);
     fd_set read_fds;
 
     while (1) {
@@ -44,23 +39,21 @@ int main (int argc, char *argv[]){
         } else if (select_return > 0) { // reading and writing
             ssize_t bytes_read = read(read_pipe, buffer, num_racks * sizeof *buffer);
             if (bytes_read > 0) {
-                // like in NEM, 1 is turn on, -1 is turn off, 0 is do nothing
                 for (int i = 0; i < num_racks; i++) {
-                    if (buffer[i] >= TURN_OFF_TEMP) {
-                        power[i] = 0;
-                    } else if (buffer[i] >= START_FAN_TEMP) {
-                        fans[i] = 1;
-                    } else if (buffer[i] <= STOP_FAN_TEMP) {
-                        fans[i] = -1;
-                    }
+                    plans[i] = 0;
+
+                    if (buffer[i] >= TURN_OFF_TEMP)
+                        plans[i] = 2;
+                    else if (buffer[i] >= START_FAN_TEMP)
+                        plans[i] = 3;
+                    else if (buffer[i] <= STOP_FAN_TEMP)
+                        plans[i] = 4;
                 }
 
                 for (int i = 0; i < num_racks; i++) {
-                    write_buffer[i] = fans[i];
-                    write_buffer[i+num_racks] = power[i];
+                    write_buffer[i] = plans[i];
                 }
-                usleep(20*1000); // NOTE: Added this delay due to issues with reading
-                write(write_pipe, write_buffer, 2 * num_racks * sizeof *write_buffer);
+                write(write_pipe, write_buffer, num_racks * sizeof *write_buffer);
             } else if (bytes_read == 0) {
                 printf("PGS read pipe closed unexpectedly\n");
             } else {
